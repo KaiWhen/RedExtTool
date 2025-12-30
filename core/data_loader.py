fca40995836c934135972d3ee563e3a4eb4033db
KinkMyBoot <petitbonhomme30@hotmail.com>
2025-12-29 19:19:22 -0500

    integrate surge manip

--- core/data_loader.py
+++ core/data_loader.py
@@ -32,16 +32,27 @@ def load_json(filename):
     return {}
 
 
-def load_all_path_data():
+def load_forest_path_data():
     path_data = {}
-    if not os.path.exists('path_data/'):
-        os.mkdir('path_data')
+    if not os.path.exists('path_data/forest/'):
+        os.mkdir('path_data/forest/')
     i = 1
-    for root, dirs, _ in os.walk('path_data/'):
+    for root, dirs, _ in os.walk('path_data/forest/'):
         for dir in dirs:
             for _, _, files in os.walk(os.path.join(root, dir)):
                 for file in files:
                     if file.endswith('.json'):
                         path_data[i] = load_json(os.path.join(root, dir, file))
                         i += 1
+    return path_data
+
+def load_surge_path_data():
+    path_data = {}
+    if not os.path.exists('path_data/surge/'):
+        os.mkdir('path_data/surge/')
+    for root, dirs, _ in os.walk('path_data/surge/'):
+        for _, _, files in os.walk(os.path.join(root)):
+            for file in files:
+                if file.endswith('surge.json'):
+                    path_data["surge"] = load_json(os.path.join(root, file))
     return path_data
